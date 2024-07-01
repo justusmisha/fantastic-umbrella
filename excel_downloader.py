8270fc956a1158a1f9e88001c4cf2491498d1a4a
00-00-00\yustu <yustus.misha10@gmail.com>
2024-07-02 01:17:02 +0300

    no message

--- excel_downloader.py
+++ excel_downloader.py
@@ -14,18 +14,7 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 
 def google_logger(SCOPES):
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=3000)
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     return creds
 
 
